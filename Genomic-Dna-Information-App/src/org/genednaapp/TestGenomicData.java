55401f3d4c2c4a94ce03c807a32eb22175e80165
abhisheksingh <abhisheksingh7212@gmail.com>
2017-05-22 20:12:32 +0530

    Added new java class named TestGenomicData containing main class and-object of GenomicData class and called method findAllGenesInFile()-to find all genomic informattion

---
+++ Genomic-Dna-Information-App/src/org/genednaapp/TestGenomicData.java
@@ -0,0 +1,12 @@
+package org.genednaapp;
+
+public class TestGenomicData {
+		public static void main(String[] args) {
+	     
+			GenomicData info=new GenomicData();
+			
+			info.findAllGenesInFile();
+			
+		}
+}
+
